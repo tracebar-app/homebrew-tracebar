17a7a28d515192ac35b92b176a29a478347692f6
Bryan Burns <bryanburns@gmail.com>
2026-04-08 16:10:18 -0700

    fix 1.2.4 release

--- Casks/tracebar.rb
+++ Casks/tracebar.rb
@@ -1,6 +1,6 @@
 cask "tracebar" do
   version "1.2.4"
-  sha256 "4c5a6f0286cac310263e5ec741dee909b4037dfbb25602c50082aa53eea51425"
+  sha256 "66ad5ad7408062b93d219e049864dcb462e68a635ce2e1c99d45fbe1ba252a98"
 
   url "https://github.com/tracebar-app/tracebar/releases/download/#{version}/TraceBar-#{version}.dmg"
   name "TraceBar"
